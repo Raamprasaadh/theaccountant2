cf18cfaed5f2a3716b85254d881bad80ff68994f
raamprasaadh <ramprasadhsurya@gmail.com>
2021-05-15 10:39:10 +0530

    Nav bar working partially

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,25 @@
-
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import Navbar from "./components/navbarComponent";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import Add from "./containers/add";
+import View from "./containers/view";
 //styles
 function App() {
   return (
-    
-      <div className="App">
-        <MuiThemeProvider>
+    <div className="App">
+      <MuiThemeProvider>
+        <Router>
+          <Switch>
+            <Route exact path="/add" component={Add} />
+            <Route exact path="/">
+              <Redirect to="/add" />
+            </Route>
+            <Route exact path="/view" component={View} />
+          </Switch>
           <Navbar />
-        </MuiThemeProvider>
-      </div>
-    
+        </Router>
+      </MuiThemeProvider>
+    </div>
   );
 }
 
--- client/src/components/navbarComponent.js
+++ client/src/components/navbarComponent.js
@@ -1,80 +1,45 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+import {makeStyles, AppBar, Toolbar, IconButton, List, ListItem, ListItemText} from '@material-ui/core';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
-import AddPage from  "../containers/add";
-import ViewPage from "../containers/view";
-const useStyles = makeStyles((theme) => ({
-  root: {
-      flexGrow:1,
-  },
-  links: {
-    marginLeft: theme.spacing(2),
-    position: 'relative',
-  },
-}));
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-};
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
-}
-const NavbarComponent = () => {
-  const [value, setValue] = React.useState(0);
+function NavbarComponent() {
+  const useStyles = makeStyles({
+    navDisplayFlex:{
+      display:'flex',
+      justifyContent:'space-between'
+    },
+    linkText:{
+      textDecoration:'none',
+      textTransform:'uppercase',
+      color:'white'
+    }
+  })
   const classes = useStyles();
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-  
-  //const preventDefault = (event) => event.preventDefault();
+  const navLinks=[
+    {title:'add transactions', path:'/add'},
+    {title:'view transactions', path:'/view'}
+  ]
   return (
-    <AppBar title="BalanceSheet">
-      <Typography className={classes.root}>
-  
-      <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
-          <Tab label="Add" {...a11yProps(0)} />
-          <Tab label="View" {...a11yProps(1)} />
-        </Tabs>
-        <TabPanel value={value} index={0}>
-        <AddPage/>
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <ViewPage/>
-      </TabPanel>       
-      </Typography>
+    <AppBar position="static">
+      <Toolbar>
+      <IconButton edge="start" color="inherit" aria-label="Logo">
+          <AccountBoxIcon fontSize="large" />
+        </IconButton>
+        <List component="nav" aria-labelledby="main-nav" className={classes.navDisplayFlex}>
+          {
+            navLinks.map(({title,path})=>
+              
+            (  <a href={path} key={title}>
+                <ListItem button>
+                <ListItemText primary={title} className={classes.linkText}/>
+                </ListItem>
+              </a>
+            ))
+          }
+        </List>
+      </Toolbar>
     </AppBar>
-  );
-};
+  )
+}
 
-export default NavbarComponent;
+export default NavbarComponent
